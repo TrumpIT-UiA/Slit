10a741dd72f0a7bf87c4b5e64836d92f06c09b36
emilr15 <emilr15@uia.no>
2017-10-25 16:17:44 +0200

    Tror Upload kun henter inn én vilkårlig Part fra welcome.jsp - sjekk doPost

--- src/main/java/fileManager/UploadServlet.java
+++ src/main/java/fileManager/UploadServlet.java
@@ -1,6 +1,7 @@
 package fileManager;
 
 //ServletImport
+
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -31,29 +32,41 @@ public class UploadServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
 
         // Lagrer filen i /tmp - en teori er at den blir lagret på den virtuelle serveren
+        //final PrintWriter writer = response.getWriter();
+        final String id = UUID.randomUUID().toString();
         final Part filePart = request.getPart("file");
-        final String fileName = getFileName(filePart);
-        InputStream filecontent;
-        final PrintWriter writer = response.getWriter();
+        //byte[] fileContent = Maybe(filePartInputStream);
 
-        // final byte[] fileContent = fraInputTilByteArray(filecontent);
-        if (fileName.endsWith(".zip")) //De første karakterene er [B@ som indikerer .zip
-        {
-            filecontent = filePart.getInputStream();
-            final byte[] fileContent = fraInputTilByteArray(filecontent);
-            final String id = UUID.randomUUID().toString();
-            FileEntity fileEntity = new FileEntity(id, fileName, fileContent);
-            //writer.print(fileEntity.getFileContent());
-            //UploadTheDamnFile(fileEntity, writer);
+        InputStream filePartInputStream;
+            try {
+                final String fileName = getFileName(filePart);
+                filePartInputStream = filePart.getInputStream();
+                if (fileName.endsWith(".zip")) {
 
-            byte[] message = fileEntity.getFileContent();
-            request.getSession().setAttribute("message", message);
-            response.sendRedirect("welcome.jsp");
+                    int bytesRead = 0;
+                    final byte[] buffer = new byte[1024];
+                    ByteArrayOutputStream fileOutPutStream = new ByteArrayOutputStream();
 
-        } else {
-            String message = "Filen må være av typen .zip!";
-            request.getSession().setAttribute("message", message);
-            response.sendRedirect("welcome.jsp");
+                    while ((bytesRead = filePartInputStream.read(buffer)) != -1) {
+                        fileOutPutStream.write(buffer, 0, bytesRead);
+                    }
+                    byte[] fileContent = fileOutPutStream.toByteArray();
+                    FileEntity fileEntity = new FileEntity(id, fileName, fileContent);
+                    //UploadTheDamnFile(fileEntity, writer);
+                    //writer.print(fileContent);
+                    String message = fileEntity.getFileContent().toString();
+                    String message2 = fileEntity.getFilename();
+                    String message3 = fileEntity.getId();
+                    request.getSession().setAttribute("message", "FileContent: " + message + "\n" + "_____FileName: " + message2 + "\n" + "______FileID: " + message3);
+                    response.sendRedirect("welcome.jsp");
+
+                } else {
+                    String message = "Filen må være av typen .zip!";
+                    request.getSession().setAttribute("message", message);
+                    response.sendRedirect("welcome.jsp");
+                }
+            } catch (IOException ioe) {
+                throw new ServletException();
         }
     }
 
@@ -69,19 +82,13 @@ public class UploadServlet extends HttpServlet {
         }
         return null;
     }
-    //Tror problemet er at kun en "Part" av filen blir hentet inn - legg while doPost
-    public static byte[] fraInputTilByteArray(InputStream filecontent) throws IOException {
-        byte[] buffer = new byte[8192];
-        int bytesRead;
-        ByteArrayOutputStream fileContentArray = new ByteArrayOutputStream();
-        while ((bytesRead = filecontent.read(buffer)) != -1) {
-            fileContentArray.write(buffer, 0, bytesRead);
-        }
-        return fileContentArray.toByteArray();
-    }
 
-    @EJB
-    FileManagerLocal fml;
+    //public byte[] Maybe(InputStream filePartInputStream) throws IOException {
+
+
+        @EJB
+        FileManagerLocal fml;
+
     public void UploadTheDamnFile(FileEntity fileEntity, PrintWriter writer) {
         if (fml.saveFile(fileEntity) == true) {
             writer.print("Suksess... Diiiiiigg");
