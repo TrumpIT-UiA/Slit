147e259f1506019259f700221588c116fc9e3b9e
Vebjørn Nordby <vebjon16@uia.no>
2017-10-31 13:18:42 +0100

    ordnet klasse for modul

--- src/main/java/moduleManagement/Module.java
+++ src/main/java/moduleManagement/Module.java
@@ -1,5 +1,31 @@
 package moduleManagement;
 
-public class Module {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Klasse som skal brukes av ModuleManagerBean for å behandle modulobjekter.
+ */
+@Entity
+public class Module implements Serializable {
+    @Id
+    @Column
+    private int moduleID;
+    @Column
+    private String moduleName;
+    @Column
+    private String learningGoals;
+    @Column
+    private String resources;
+    @Column
+    private String tasks;
+    @Column
+    private String approvalCriteria;
+
+    public Module(){
+
+    }
 
 }
