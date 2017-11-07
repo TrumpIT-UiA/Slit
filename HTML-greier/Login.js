e85bcdad90f77ead9028942829af6a53e265bc77
ticolay <nicolayleknes@live.no>
2017-11-07 11:16:34 +0100

    123

---
+++ HTML-greier/Login.js
@@ -0,0 +1,21 @@
+/* Dette var bare en testklasse for js.
+<html>
+<head>
+    <script type="text/javascript">
+        function validator(Nicolay){
+        if v{
+        alert("Passord godkjent");
+    } else {
+        alert("Skriv inn passordet");
+    }
+    }
+</script>
+</head>
+<body>
+<form name="Nicolay">
+    <input type="checkbox" name="thebox" />
+    <input type="button" value ="pressme" onclick="validator()" />
+</form>
+</body>
+</html>
+*/
