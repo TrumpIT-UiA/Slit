596b0f6257a7307169a1e6e83a233f7c571e6a2f
ticolay <nicolayleknes@live.no>
2017-11-17 20:49:11 +0100

    Update - session & og shit

---
+++ src/main/java/users/SessionID.java
@@ -0,0 +1,30 @@
+
+package users;
+
+import javax.ejb.EJB;
+/*
+@EJB
+private void createNewId (HttpServletRequest request, HttpServletResponse response) throws IOException {
+        {
+        if (user == null) {
+        Principal principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+        if (principal != null) {
+        user = userService.find(principal.getName()); // Find User by j_username.
+        }
+        }
+        return user;
+        }
+
+        }
+        }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        createNewStudent(request, response);
+    }
+}
+*/
