2630f623cea1ab7c594e3e1ab8154613f87ed8c5
Konflux <marign16@student.uia.no>
2017-12-13 10:23:14 +0100

    Preppet master

--- src/main/java/feedbackManagement/FeedbackManagerLocal.java
+++
@@ -1,9 +0,0 @@
-package feedbackManagement;
-
-import javax.ejb.Local;
-
-public interface FeedbackManagerLocal {
-    Feedback getFeedback(String primaryChunk);
-    boolean saveFeedback(Feedback f);
-    boolean updateFeedback(Feedback f);
-}
